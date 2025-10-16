fd52f65a9011e2911d450c47de571bae0b556679
matsu182 <amt1448@gmail.com>
2025-10-16 21:42:03 +0900

    api連携のソースに変更

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+
+function App() {
+	const [data, setData] = React.useState();
+	const url = "https://test1-back-1049817508063.asia-northeast1.run.app";
+
+	const GetData = () => {
+		axios.get(url).then((res) => {
+			setData(res.data);
+		});
+	};
+	return (
+		<div>
+			<div>ここに処理を書いていきます</div>
+			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+		</div>
+	);
+}
+
+export default App;
